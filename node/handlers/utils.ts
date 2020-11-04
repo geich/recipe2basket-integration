4b4d7667158b68175c3d804dcca7e524c8c59ebd
Gabriel Eich Moraes <eich.gabriel1@gmail.com>
2020-11-04 15:19:53 -0300

    parents ids in categories

--- node/handlers/utils.ts
+++ node/handlers/utils.ts
@@ -1,8 +1,20 @@
+const createParentsId = (i: any, elem: any) => {
+    const levelName = `l${i}`
+    elem.parentsIds = elem.parentsIds || {}
+    elem.parentsIds[levelName] = elem.id
+
+    return elem.parentsIds
+}
+
+
 export const mapCategories = (categories: object[], level: number) => {
     for(let i = 1; i < level; i++) {
         categories = categories.reduce((acc: object[], elem: any) => {
             acc = acc || []
-            elem.children.map((e: object) => acc.push(e))
+            elem.children.map((e: any) => acc.push({ 
+                ...e, 
+                parentsIds: createParentsId(i, elem) 
+            }))
             return acc
         }, [])
     }
